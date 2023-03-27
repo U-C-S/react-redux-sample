d6be5ae56929514018a01209a6760039cb250a2b
U-C-S <uchanakyasrinivas@gmail.com>
2023-03-27 12:56:18 +0530

    remove boilerplate files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { MantineProvider, Text } from "@mantine/core";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
